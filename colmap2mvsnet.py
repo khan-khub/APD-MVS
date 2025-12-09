b1f58e1c0ded0f1616a36a6e6982c4afb0c2bf4e
khan-khub <khan.khub@northeastern.edu>
2025-12-09 18:25:16 -0500

    update ignore update cmake list for RTX5080 fix numpy req

--- colmap2mvsnet.py
+++ colmap2mvsnet.py
@@ -373,7 +373,7 @@ def processing_single_scene(args):
             if p3d_id == -1:
                 continue
             transformed = np.matmul(extrinsic[i+1], [points3d[p3d_id].xyz[0], points3d[p3d_id].xyz[1], points3d[p3d_id].xyz[2], 1])
-            zs.append(np.asscalar(transformed[2]))
+            zs.append(transformed[2].item())
         depth_min = 0
         depth_max = 0
         if len(zs) != 0:
